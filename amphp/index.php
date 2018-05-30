e8453978fa148d943b4fd14fcd87b04aa81bb268
Joaquín Fernández Campo <jfcampo@gmail.com>
2018-05-30 14:26:39 +0200

    AMPHP in progress

---
+++ amphp/index.php
@@ -0,0 +1,34 @@
+<?php
+
+use Amp\Artax\Response;
+use Amp\Loop;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Loop::run(function () {
+	    $uris = [
+		            "https://google.com/",
+			            "https://github.com/",
+				            "https://stackoverflow.com/",
+					        ];
+
+	        $client = new Amp\Artax\DefaultClient;
+	        $client->setOption(Amp\Artax\Client::OP_DISCARD_BODY, true);
+
+		    try {
+			            foreach ($uris as $uri) {
+					                $promises[$uri] = $client->request($uri);
+							        }
+
+				            $responses = yield $promises;
+
+				            foreach ($responses as $uri => $response) {
+						                print $uri . " - " . $response->getStatus() . $response->getReason() . PHP_EOL;
+								        }
+					        } catch (Amp\Artax\HttpException $error) {
+							        // If something goes wrong Amp will throw the exception where the promise was yielded.
+							//         // The Client::request() method itself will never throw directly, but returns a promise.
+							//                 print $error->getMessage() . PHP_EOL;
+							//                     }
+							//                     });
+							//                     }
